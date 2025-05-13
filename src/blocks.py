1c0efdffdb4197b305e8a4b094ee513977f88f52
fatih <rinqtf@gmail.com>
2025-05-14 00:08:15 +0300

    add block to block type

--- src/blocks.py
+++ src/blocks.py
@@ -1,6 +1,32 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(md):
     blocks = md.split("\n\n")
     blocks = list(map(lambda x: x.strip(), blocks))
     blocks = list(filter(lambda x: x != "", blocks))
 
     return blocks
+
+
+def block_to_blocktype(block):
+    if block.startswith("#"):
+        return BlockType.HEADING
+    elif block.startswith("```"):
+        return BlockType.CODE
+    elif block.startswith(">"):
+        return BlockType.QUOTE
+    elif block.startswith("-"):
+        return BlockType.UNORDERED_LIST
+    elif block[0].isdigit() and block[1] == ".":
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from blocks import markdown_to_blocks
+from blocks import BlockType, block_to_blocktype, markdown_to_blocks
 
 
 class TestBlocks(unittest.TestCase):
@@ -23,6 +23,24 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_heading(self):
+        self.assertEqual(block_to_blocktype("# Heading"), BlockType.HEADING)
+
+    def test_code(self):
+        self.assertEqual(block_to_blocktype("```code block```"), BlockType.CODE)
+
+    def test_quote(self):
+        self.assertEqual(block_to_blocktype("> Quote"), BlockType.QUOTE)
+
+    def test_unordered_list(self):
+        self.assertEqual(block_to_blocktype("- List item"), BlockType.UNORDERED_LIST)
+
+    def test_ordered_list(self):
+        self.assertEqual(block_to_blocktype("1. List item"), BlockType.ORDERED_LIST)
+
+    def test_paragraph(self):
+        self.assertEqual(block_to_blocktype("Just a paragraph"), BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
