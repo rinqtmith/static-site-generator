d47b997a038649ef3fb3cb50a6f7cfe416a11a39
fatih <rinqtf@gmail.com>
2025-05-15 00:52:34 +0300

    possible fixes

--- src/copystatic.py
+++ src/copystatic.py
@@ -4,7 +4,7 @@ import shutil
 
 def copy_files_recursive(source_dir_path, dest_dir_path):
     if not os.path.exists(dest_dir_path):
-        os.mkdir(dest_dir_path)
+        os.makedirs(dest_dir_path)
 
     for filename in os.listdir(source_dir_path):
         from_path = os.path.join(source_dir_path, filename)
--- src/main.py
+++ src/main.py
@@ -6,13 +6,13 @@ from copystatic import copy_files_recursive
 from generate import generate_pages_recursive
 
 
-dir_path_static = "static"
-dir_path_public = "docs"
+dir_path_static = "./static"
+dir_path_public = "./docs"
 from_path = "content/index.md"
 dest_path = "docs/index.html"
-template_path = "template.html"
-dir_path_content = "content"
-dest_dir_path = "docs"
+template_path = "./template.html"
+dir_path_content = "./content"
+dest_dir_path = "./docs"
 
 
 def main():
@@ -22,18 +22,16 @@ def main():
         basepath = "/"
 
     print("Deleting public directory...")
-    if os.path.exists(f".{basepath}{dir_path_public}"):
-        shutil.rmtree(f".{basepath}{dir_path_public}")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
     print("Copying static files to public directory...")
-    copy_files_recursive(
-        f".{basepath}{dir_path_static}", f".{basepath}{dir_path_public}"
-    )
+    copy_files_recursive(dir_path_static, dir_path_public)
 
     generate_pages_recursive(
-        f".{basepath}{dir_path_content}",
-        f".{basepath}{template_path}",
-        f".{basepath}{dest_dir_path}",
+        dir_path_content,
+        template_path,
+        dir_path_public,
         basepath,
     )
 
