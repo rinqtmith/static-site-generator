5e7aae49f6959f0d1c348a29864cec24ab8a21ab
fatih <rinqtf@gmail.com>
2025-05-15 00:13:59 +0300

    use basepath for folders

--- src/main.py
+++ src/main.py
@@ -1,28 +1,38 @@
 import os
+import sys
 import shutil
 
 from copystatic import copy_files_recursive
 from generate import generate_pages_recursive
 
 
-dir_path_static = "./static"
-dir_path_public = "./public"
-from_path = "./content/index.md"
-dest_path = "./public/index.html"
-template_path = "./template.html"
-dir_path_content = "./content"
-dest_dir_path = "./public"
+dir_path_static = "static"
+dir_path_public = "public"
+from_path = "content/index.md"
+dest_path = "public/index.html"
+template_path = "template.html"
+dir_path_content = "content"
+dest_dir_path = "public"
 
 
 def main():
+    try:
+        basepath = sys.argv[1]
+    except IndexError:
+        basepath = "./"
+
     print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    if os.path.exists(f"{basepath}{dir_path_public}"):
+        shutil.rmtree(f"{basepath}{dir_path_public}")
 
     print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
+    copy_files_recursive(f"{basepath}{dir_path_static}", f"{basepath}{dir_path_public}")
 
-    generate_pages_recursive(dir_path_content, template_path, dest_dir_path)
+    generate_pages_recursive(
+        f"{basepath}{dir_path_content}",
+        f"{basepath}{template_path}",
+        f"{basepath}{dest_dir_path}",
+    )
 
 
 main()
